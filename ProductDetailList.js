8e2c2abf244ad970dff40bca1308e6f176b38793
artilleur <99259153+artilleur@users.noreply.github.com>
2023-12-20 10:30:18 +0100

    20122023   2

--- ProductDetailList.js
+++ ProductDetailList.js
@@ -1,26 +1,29 @@
 // ProductDetailList.js
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 
 const ProductDetailList = ({ route }) => {
   const { product } = route.params;
 
   return (
-    <View style={{ alignItems: 'center', marginBottom: 10 }}>
-      <Image
-        source={{
-          uri: `http://10.0.2.2:8000/images/${product.image}`,
-        }}
-        style={{ width: 150, height: 150 }}
-      />
-      <View style={{ alignItems: 'center', marginBottom: 10 }}></View>
-      <Text style={{ color: 'red' }}>NOM</Text>
-      <Text>{product.nom} {'\n'} </Text>
-      <Text style={{ color: 'red' }}>PRIX {'\n'}</Text>
-      <Text>{product.prix}€ {'\n'} {'\n'} {'\n'} </Text>
-      <Text>{product.description} {'\n'} {'\n'} {'\n'} </Text>
-      {/* You can display other product details here */}
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={{ alignItems: 'center', marginBottom: 10 }}>
+        <Image
+          source={{
+            uri: `http://10.0.2.2:8000/images/${product.image}`,
+          }}
+          style={{ width: 150, height: 150 }}
+        />
+        <View style={{ alignItems: 'center', marginBottom: 10, marginHorizontal: 35 }}>
+          <Text style={{ color: 'red' }}>NOM</Text>
+          <Text>{product.nom} {'\n'} </Text>
+          <Text style={{ color: 'red' }}>PRIX {'\n'}</Text>
+          <Text>{product.prix}€ {'\n'} {'\n'} {'\n'} </Text>
+          <Text>{product.description} {'\n'} {'\n'} {'\n'} </Text>
+          {/* You can display other product details here */}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
